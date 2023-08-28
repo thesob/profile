8c5b67b76384640bc353eea3e113febffcc91c12
Patricio Sobrado <patricio.sobrado@gmail.com>
2023-08-28 16:00:00 -0400

    More project pages

--- src/content/config.ts
+++ src/content/config.ts
@@ -9,26 +9,23 @@ const blogSchema = z.object({
     badge: z.string().optional(),
 });
 
-const storeSchema = z.object({
+const projectSchema = z.object({
     title: z.string(),
     description: z.string(),
-    custom_link_label: z.string(),
-    custom_link: z.string().optional(),
+    visit_link_label: z.string(),
+    visit_link: z.string().optional(),
     updatedDate: z.coerce.date(),
-    pricing: z.string().optional(),
-    oldPricing:  z.string().optional(),
     badge: z.string().optional(),
-    checkoutUrl: z.string().optional(),
     heroImage: z.string().optional(),
 });
 
 export type BlogSchema = z.infer<typeof blogSchema>;
-export type StoreSchema = z.infer<typeof storeSchema>;
+export type ProjectSchema = z.infer<typeof projectSchema>;
 
 const blogCollection = defineCollection({ schema: blogSchema });
-const storeCollection = defineCollection({ schema: storeSchema });
+const projectCollection = defineCollection({ schema: projectSchema });
 
 export const collections = {
     'blog': blogCollection,
-    'store': storeCollection
+    'project': projectCollection
 }
