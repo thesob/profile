82a89eff1b755b5f674d9b626a10abc11f702c0e
Patricio Sobrado <patricio.sobrado@gmail.com>
2023-12-30 00:59:14 -0300

    Fix date data type on blogSchema

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,7 +4,7 @@ const blogSchema = z.object({
     title: z.string(),
     description: z.string(),
     pubDate: z.coerce.date(),
-    updatedDate: z.string().optional(),
+    updatedDate: z.coerce.date().optional(),
     heroImage: z.string().optional(),
     badge: z.string().optional(),
     tags: z.array(z.string()).optional()
